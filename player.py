7fc7a32ddedba061a944679059fed30a4feab76f
siemaeniu500 <siemaeniu500@gmail.com>
2017-10-23 20:16:52 +0200

    dadsad

--- player.py
+++ player.py
@@ -4,12 +4,14 @@ from ship import *
 class Player():
 
     def __init__(self, name:str, is_human:bool, ocean:Ocean):
+        self.ocean = ocean
+        self.is_human = is_human
         self.name = name
         self.start_time = time()
 
     def add_ship_to_ocean(self):
-        ca = Carrier(self.is_human)
-        ba = BattleShip(self.is_human)
-        cr = Cruiser(self.is_human)
-        su = Submarine(self.is_human)
-        de = Destroyer(self.is_human)
+        ca = Carrier(self.is_human, self.ocean)
+        ba = BattleShip(self.is_human, self.ocean)
+        cr = Cruiser(self.is_human, self.ocean)
+        su = Submarine(self.is_human, self.ocean)
+        de = Destroyer(self.is_human, self.ocean)
