dc62a9dc4400686b585282a6a32c51134b3027ed
siemaeniu500 <siemaeniu500@gmail.com>
2017-10-24 07:54:55 +0200

    add dificulty level parameter do SingleGame class constructor

--- game.py
+++ game.py
@@ -15,12 +15,13 @@ class Game():
 
 class SingleGame(Game):
 
-    def __init__(self, player_name:str):
+    def __init__(self, player_name:str, difficulty_level:int):
+        self.difficulty_level = difficulty_level #difficulty level where 0 = easy, 1 = medium, 2 = hard
         self.player_name
         player_ocean = Ocean()
-        human_player = Player(player_name,True , player_ocean)
+        human_player = Player(player_name, True , player_ocean)
         bot_ocean = Ocean()
-        bot_player = Player('Bot', False, bot_ocean)
+        bot_player = Player('Bot',False, bot_ocean)
 
 
     def start_game(self):
