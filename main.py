3d4d4ee7d62c75496032318a670a9aac9ce9aa9c
creat73 <nikill1125@gmail.com>
2017-10-24 11:41:22 +0200

    Deleted new line sign at the and of each line in import

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from game import *
 
 
 def import_highscore():
-
+    new_line_position = -1
     highscore = []
 
     with open('highscore.csv', 'r') as file:
         for line in file:
-            highscore.append(line.split(','))
-    for line in highscore:
-        print(line)
+            highscore.append(line[:new_line_position].split(','))
     return highscore
 
 
@@ -27,16 +25,27 @@ def main():
 
     while True:
         option = input('''
-        Choose option:
-        (1) Start single player
-        (2) Start multi player
-        (3) Show highscores
-        (4) exit''')
+Choose option:
+(1) Start single player
+(2) Start multi player
+(3) Show highscores
+(4) exit''')
 
         if option == '1':
-            ...
+            name = input('Type in your name: ')
+            difficulity_level = int(input('''
+            
+Select difficulity:
+(1) Easy
+(2) Medium
+(3) Hard'''))
+
         elif option == '2':
-            ...
+            first_player_name = input('Type in first player name: ')
+            second_player_name = input('Type in second player name: ')
+
+            MultiPlayerGame(first_player_name, second_player_name)
+
         elif option == '3':
             table = Texttable()
             table.set_deco(Texttable.HEADER)
