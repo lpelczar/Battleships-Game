5df262ba5c48d8199b987da8e627f83f28980bf0
lpelczar <lukepelczar@gmail.com>
2017-10-26 10:10:26 +0200

    Add docstringsto Square Class

--- square.py
+++ square.py
@@ -1,27 +1,51 @@
 class Square:
-
+    """
+    Abstract class for representation of a single Square on board
+    """
     def __init__(self, sign):
+        """
+        :param sign: str -> String representation of square
+        """
         self.sign = sign
         self.final_sign = sign
 
     def change_sign(self, sign):
+        """
+        Change the sign of a Square object
+
+        :param sign: str -> String representation of square
+        """
         self.sign = sign
 
     def __repr__(self):
+        """
+        Return string representation of a Square
+        """
         return self.sign
 
     def __str__(self):
+        """
+        Return string representation of a Square
+        """
         return self.sign
 
 
 class BorderSquare(Square):
+    """
+    Concrete class representing the Border
+    """
     def __init__(self):
         super().__init__("~")
 
 
 class OceanSquare(Square):
-
+    """
+    Concrete class representing the playable Ocean
+    """
     def __init__(self, ShipReference=None):
+        """
+        :param ShipReference: str -> String representation of OceanSquare
+        """
         if(ShipReference is None):
             super().__init__(" ")
         else:
@@ -29,6 +53,11 @@ class OceanSquare(Square):
 
 
 class ShipSquare(Square):
+    """
+    Concrete class representing the Ship
+    """
     def __init__(self, sign):
+        """
+        :param sign: str -> String representation of square
+        """
         super().__init__(sign)
-
